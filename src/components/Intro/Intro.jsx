328106ffe7d9e015629e0788dd89cb23303c685c
Gustavo Godoy <135852967+GustavoGodoy12@users.noreply.github.com>
2024-12-04 17:45:18 -0300

    new

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,61 +1,69 @@
 import React, { useContext } from "react";
 import "./Intro.css";
-import Vector1 from "../../img/Vector1.png";
-import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
-import thumbup from "../../img/prevention 1.png";
-import crown from "../../img/crown.png";
-import FloatinDiv from "../FloatingDiv/FloatingDiv";
-import Github from "../../img/github.png";
-import SaudeIcon from "../../img/cuidados-de-saude 1 (2).png";
-import Marcus from "../../img/marcus_com_fundo-removebg-preview.png";
-import LinkedIn from "../../img/linkedin.png";
+import Marcus from "../../img/marcus com fundo.jpeg";
 import Instagram from "@iconscout/react-unicons/icons/uil-instagram";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 import Whats from "@iconscout/react-unicons/icons/uil-whatsapp";
+
 const Intro = () => {
   // Transition
   const transition = { duration: 2, type: "spring" };
 
-  // context
+  // Context
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
 
   return (
     <div className="Intro" id="Intro">
-      {/* left name side */}
+      {/* Left side (hidden on small screens) */}
       <div className="i-left">
         <div className="i-name">
-          {/* yahan change hy darkmode ka */}
-          <span style={{ color: darkMode ? "white" : "" }}>Seja bem-vindo</span>
-          <span>Doutor Marcus Winheski</span>
-          <span>
-            Médico especialista em Cirurgia do Câncer, Prevenção e Tratamento Oncológico
+          <span className="welcome-text" style={{ color: darkMode ? "white" : "" }}>
+            Seja bem-vindo
           </span>
+          <span className="doctor-name">Doutor Marcus Winheski</span>
         </div>
         <Link to="experience" smooth={true} spy={true}>
           <button className="button i-button">Ver mais</button>
         </Link>
-        {/* social icons */}
         <div className="i-icons">
-        <Whats color="#FCA61F" size={"5rem"} />
-        <Instagram color="#FCA61F" size={"5rem"} />
-          
+          <Whats color="#FCA61F" size={"5rem"} />
+          <Instagram color="#FCA61F" size={"5rem"} />
         </div>
       </div>
-      {/* right image side */}
+
+      {/* Right side */}
       <div className="i-right">
-        <img src={Vector1} alt="" />
-        <img src={Vector2} alt="" />
-        <img src={Marcus} alt="" />
-        {/* animation */}
+        {/* Image container with gradient */}
+        <div className="image-container">
+          <img src={Marcus} alt="Doutor Marcus Winheski" className="marcus-image" />
+          <div className="gradient-overlay"></div>
+        </div>
+
+        {/* Título e descrição abaixo da imagem */}
+        <div className="description-text">
+          <strong>Dr Marcus Winheski</strong>
+          <p>Médico especialista em Cirurgia do Câncer, Prevenção e Tratamento Oncológico</p>
+        </div>
+
+        {/* Espaçamento de 50px */}
+        <div className="spacing"></div>
+
+        {/* Ícones abaixo da descrição */}
+        <div className="i-icons-mobile">
+          <Whats color="#FCA61F" size={"3rem"} />
+          <Instagram color="#FCA61F" size={"3rem"} />
+        </div>
+
+        {/* Animações (opcional) */}
         <motion.img
           initial={{ left: "-36%" }}
           whileInView={{ left: "-24%" }}
           transition={transition}
           alt=""
+          className="motion-img"
         />
 
         <motion.div
@@ -64,23 +72,22 @@ const Intro = () => {
           transition={transition}
           className="floating-div"
         >
-          <FloatinDiv img={SaudeIcon} text1="Saúde" text2="" />
+          {/* Conteúdo adicional, se necessário */}
         </motion.div>
 
-        {/* animation */}
         <motion.div
           initial={{ left: "9rem", top: "18rem" }}
           whileInView={{ left: "0rem" }}
           transition={transition}
           className="floating-div"
         >
-          {/* floatinDiv mein change hy dark mode ka */}
-          <FloatinDiv img={thumbup} text1="Prevenção" text2="" />
+          {/* Conteúdo adicional, se necessário */}
         </motion.div>
 
-        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        {/* Gradientes adicionais */}
+        <div className="blur background-blur"></div>
         <div
-          className="blur"
+          className="blur additional-blur"
           style={{
             background: "#C1F5FF",
             top: "17rem",
